52269f244fd5c92cebcc671abbb7d357ecf89138
Julz <50104947+danisnotonzebra1@users.noreply.github.com>
2019-11-23 22:26:15 +0100

    Update ClientHandler.java

--- Multithreaded_server/src/Cat/chat/mini/project/aau/med3/ClientHandler.java
+++ Multithreaded_server/src/Cat/chat/mini/project/aau/med3/ClientHandler.java
@@ -9,13 +9,13 @@ class ClientHandler extends Thread {
 
     final DataInputStream dis;
     final DataOutputStream dos;
-    final Socket s;
-    int whichClient;
+    final Socket socket;
+    final int whichClient;
 
 
     // Constructor
-    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos, int whichClient) {
-        this.s = s;
+    public ClientHandler(Socket socket, DataInputStream dis, DataOutputStream dos, int whichClient) {
+        this.socket = socket;
         this.dis = dis;
         this.dos = dos;
         this.whichClient = whichClient;
@@ -24,31 +24,41 @@ class ClientHandler extends Thread {
 
     @Override
     public void run() {
+        
         String received;
-        String toreturn;
+        String tosend;
         String username = null;
+        
         try {
+            
+            // sends initial message to the client that greets the client to the server
             dos.writeUTF("Hello and welcome to the chat!");
+            // retrieves the username from the client
             username = dis.readUTF();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+        
         while (true) {
             try {
-                // Ask user what he wants
-                // receive the answer from client
+                // thread waits to receive a message from the client
                 received = dis.readUTF();
+                // prints the message to the console that the server received the message
                 System.out.println("Message received from client: " + whichClient + " '" + received + "'");
-
+                
+                // creates a string with a passed username of a client and the received message
                 toreturn = (username + ": " + received);
-                Server.broadcast(toreturn, whichClient);
+                // sends that message to all the clients except the current client
+                Server.broadcast(tosend, whichClient);
 
-
+                // if exit is received then the thread closes the socket and ends the while loop
                 if (received == "Exit") {
-                    System.out.println("Client " + this.s + " sends exit...");
+                    System.out.println("Client " + this.socket + " sends exit...");
                     System.out.println("Closing this connection.");
-                    this.s.close();
-                    break;
+                    this.socket.close(); // closes the socket
+                    break; // breaks the loop
                 }
 
             } catch (IOException e) {
@@ -57,7 +67,7 @@ class ClientHandler extends Thread {
         }
 
         try {
-            // closing resources
+            // to close resources
             this.dis.close();
             this.dos.close();
 
