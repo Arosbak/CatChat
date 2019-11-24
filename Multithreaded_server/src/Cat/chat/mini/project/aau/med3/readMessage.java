0fd2c02f4b25344ed27344af6dea73be22a43b8b
ArijusGrotuzas <50104866+ArijusGrotuzas@users.noreply.github.com>
2019-11-24 09:57:05 +0100

    Update readMessage.java

--- Multithreaded_server/src/Cat/chat/mini/project/aau/med3/readMessage.java
+++ Multithreaded_server/src/Cat/chat/mini/project/aau/med3/readMessage.java
@@ -4,24 +4,25 @@ import javax.swing.*;
 import java.io.DataInputStream;
 import java.io.IOException;
 
+// class for receiving messages from the server
 class readMessage extends Thread{
     final DataInputStream dis;
     final JTextArea chatText;
 
     public readMessage(DataInputStream dis, JTextArea chatText){
-        this.dis = dis;
-        this.chatText = chatText;
+        this.dis = dis; // setting passed input stream as the instance
+        this.chatText = chatText; // setting passed JtextArea as the instance 
     }
 
     @Override
     public void run() {
         while(true){
-            String received;
+            String received; // string for received message
 
             try {
-                received = dis.readUTF();
-                chatText.append(received  + "\n");
-                System.out.println(received);
+                received = dis.readUTF(); // getting the message from the server
+                chatText.append(received  + "\n"); // appending it to the TextArea
+                System.out.println(received); // printing out to the client
 
 
             } catch (IOException e) {
